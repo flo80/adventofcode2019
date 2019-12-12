3662a34d49f4430ede2f92abe10e6acbf511d3d1
Florian Lloyd-Pötscher <florian.poetscher@gmail.com>
2019-12-12 13:46:01 +0100

    day3 integrated into tests and run

--- aoc2019/app/Main.hs
+++ aoc2019/app/Main.hs
@@ -1,6 +1,12 @@
 module Main where
 
+import           AOC2019.Day3
 import           AOC2019.Day12
 
 main :: IO ()
-main = day12run
+main = do
+  putStrLn "\n==================="
+  putStrLn "Advent of Code 2019"
+  putStrLn "===================\n"
+  day3run
+  day12run
--- aoc2019/test/Spec.hs
+++ aoc2019/test/Spec.hs
@@ -1,5 +1,6 @@
 import           Test.HUnit
+import           AOC2019.Test.Day3
 import           AOC2019.Test.Day12
 
 main :: IO Counts
-main = runTestTT $ TestList [day12tests]
+main = runTestTT $ TestList [day3tests, day12tests]
