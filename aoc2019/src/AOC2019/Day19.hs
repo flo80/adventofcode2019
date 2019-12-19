9f454e4ee2b80a00f722f7308da84e9dea4caac6
Florian Lloyd-Pötscher <florian.poetscher@gmail.com>
2019-12-19 14:25:07 +0100

    slow version - not working

--- aoc2019/src/AOC2019/Day19.hs
+++ aoc2019/src/AOC2019/Day19.hs
@@ -5,7 +5,14 @@ module AOC2019.Day19
   )
 where
 
-import           AOC2019.IntCodeComputer
+import           AOC2019.IntCodeComputer                  ( interactiveComputer
+                                                          )
+import           Data.List.Split                          ( chunksOf )
+import           Data.List                                ( all )
+import           Data.Map                                 ( Map )
+import qualified Data.Map                      as Map
+import           Control.Monad.State
+import           Debug.Trace
 
 day19run :: IO ()
 day19run = do
@@ -17,13 +24,65 @@ day19run = do
   putStrLn ""
 
 
-parseInput :: String -> String
-parseInput = id
-
 day19a :: String -> Int
-day19a contents = sum $ concatMap
-  (\(a, b) -> interactiveComputer contents [a, b])
-  [ (a, b) | a <- [0 .. 49], b <- [0 .. 49] ]
+day19a = sum . getDroneOutput1
+
+getDroneOutput1 :: String -> [Int]
+getDroneOutput1 contents = concatMap
+  (\(x, y) -> interactiveComputer contents [x, y])
+  [ (x, y) | y <- [0 .. 49], x <- [0 .. 49] ]
+
 
 day19b :: String -> Int
-day19b = undefined
+day19b contents = (10000 * x + y)
+ where
+    -- setInitialState contents Map.empty
+  (x, y) = findArea (0, 10)
+
+  findArea :: Pos -> Pos
+  findArea (testX, currY) = result
+   where
+        -- firstX follow the line to reduce search
+    (firstX, _)  = findFirstX (testX, currY)
+    (lastX , _)  = findLastX (firstX, currY)
+    searchResult = searchArea firstX lastX currY
+
+    result       = case (lastX - firstX) < 10 of
+      True  -> findArea (firstX, currY + 1)
+      False -> case searchResult of
+        Just pos -> pos
+        Nothing  -> findArea (firstX, currY + 1)
+
+
+  searchArea :: Int -> Int -> Int -> Maybe Pos
+  searchArea currX lastX currY | currX + 10 > lastX = Nothing
+                               | otherwise          = result
+   where
+    searchField =
+      [ (x, y) | y <- [currY .. (currY + 9)], x <- [currX .. (currX + 9)] ]
+    searchResults = map getDroneOutput searchField
+    result        = case all (== 1) searchResults of
+      True  -> Just (currX, currY)
+      False -> searchArea (currX + 1) lastX currY
+
+
+  findFirstX :: Pos -> Pos
+  findFirstX (testX, currY) = case getDroneOutput (testX, currY) == 1 of
+    True  -> (testX, currY)
+    False -> findFirstX ((testX + 1), currY)
+
+  findLastX :: Pos -> Pos
+  findLastX (testX, currY) = result
+   where
+    test   = getDroneOutput (testX, currY)
+
+    result = case test == 0 of
+      True  -> (testX - 1, currY)
+      False -> findLastX ((testX + 1), currY)
+
+
+  getDroneOutput :: Pos -> Int
+  getDroneOutput (x, y) =
+    head $ memoize 2 $ (interactiveComputer contents $ traceShowId  [x, y])
+
+type Pos = (Int, Int)
