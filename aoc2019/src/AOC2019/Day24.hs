559894dd9ad2dc082f661eda3a6ddb17f1119380
Florian Lloyd-Pötscher <florian.poetscher@gmail.com>
2020-01-06 21:31:05 +0100

    Day24 part 2 (slow but works)

--- aoc2019/src/AOC2019/Day24.hs
+++ aoc2019/src/AOC2019/Day24.hs
@@ -6,6 +6,10 @@ module AOC2019.Day24
 where
 
 import           AOC.Common
+import           Data.Maybe                               ( fromMaybe )
+import           Data.List                                ( (\\)
+                                                          , foldl'
+                                                          )
 import           Data.Map                                 ( Map )
 import qualified Data.Map                      as Map
 import           Data.Set                                 ( Set )
@@ -18,7 +22,7 @@ day24run = do
   putStr "Day 24 - Part 1: "
   print $ day24a contents
   putStr "Day 24 - Part 2: "
-  print $ day24b contents
+  print $ day24b 200 contents
   putStrLn ""
 
 
@@ -37,6 +41,11 @@ instance Enum Tile where
 
 parseInput :: String -> Map Position Tile
 parseInput = parseTilesFromString
+
+
+day24a :: String -> Int
+day24a = bioDiversity . findRepetition . parseInput
+
 
 nextState :: Map Position Tile -> Map Position Tile
 nextState tiles = Map.mapWithKey evolution tiles
@@ -62,8 +71,72 @@ bioDiversity tiles = sum $ map score liveTiles
   liveTiles = Map.keys $ Map.filter (== Alive) tiles
   score (V2 x y) = 2 ^ (x + 5 * y)
 
-day24a :: String -> Int
-day24a = bioDiversity . findRepetition . parseInput
 
-day24b :: String -> Int
-day24b = undefined
+day24b :: Int -> String -> Int
+day24b iterations contents = count $ iterate $ addLevel $ parseInput contents
+ where
+  addLevel :: Map Position Tile -> Map (Int, Position) Tile
+  addLevel = Map.fromList . map (\(pos, v) -> ((0, pos), v)) . Map.toList
+
+  iterate tiles = foldl' (\t _ -> nextStateMulti t) tiles [1 .. iterations]
+  count = Map.size . Map.filter (== Alive)
+
+
+nextStateMulti :: Map (Int, Position) Tile -> Map (Int, Position) Tile
+nextStateMulti tiles = Map.mapWithKey evolution relevantTiles
+ where
+  aliveTiles = Map.filter (== Alive) tiles
+  allNeighbors =
+    Map.fromList $ map (\p -> (p, Dead)) $ concatMap adjacent $ Map.keys
+      aliveTiles
+  -- combine neighbors and existing active tiles
+  relevantTiles = Map.union aliveTiles allNeighbors
+
+  evolution (level, position) state = case state of
+    Alive -> if nbs == 1 then Alive else Dead
+    Dead  -> if nbs == 1 || nbs == 2 then Alive else Dead
+   where
+    adj = adjacent (level, position)
+    nbs = Map.size
+      $ Map.filterWithKey (\k v -> k `elem` adj && v == Alive) relevantTiles
+
+  adjacent :: (Int, Position) -> [(Int, Position)]
+  adjacent (_, V2 2 2) = error "requesting neighbor for middle"
+  adjacent p@(level, V2 2 1) =
+    direct p ++ map (\x -> (level + 1, V2 x 0)) [0 .. 4]
+  adjacent p@(level, V2 2 3) =
+    direct p ++ map (\x -> (level + 1, V2 x 4)) [0 .. 4]
+  adjacent p@(level, V2 1 2) =
+    direct p ++ map (\y -> (level + 1, V2 0 y)) [0 .. 4]
+  adjacent p@(level, V2 3 2) =
+    direct p ++ map (\y -> (level + 1, V2 4 y)) [0 .. 4]
+  adjacent p@(level, position) = direct p
+
+  direct (level, position) =
+    map adjustLevel
+      $  (map (\x -> (level, x + position)) adjacentPositions)
+      \\ [(level, V2 2 2)]
+   where
+      -- outer borders
+    adjustLevel :: (Int, Position) -> (Int, Position)
+    adjustLevel (level, V2 (-1) y) = (level - 1, V2 1 2) -- left
+    adjustLevel (level, V2 5 y   ) = (level - 1, V2 3 2) -- right
+    adjustLevel (level, V2 x (-1)) = (level - 1, V2 2 1) -- top
+    adjustLevel (level, V2 x 5   ) = (level - 1, V2 2 3) -- bottom
+    adjustLevel p                  = p
+
+
+printAll :: Map (Int, Position) Tile -> String
+printAll tiles = concatMap
+  (\l -> "Level " ++ show l ++ "\n" ++ printLevel tiles l ++ "\n\n")
+  [minLevel .. maxLevel]
+ where
+  (minLevel, maxLevel) =
+    foldl (\(minL, maxL) (l, p) -> (min minL l, max maxL l)) (0, 0)
+      $ Map.keys tiles
+
+  printLevel tiles level = concatMap row [0 .. 4]
+   where
+    row y = (concatMap (\x -> get x y) [0 .. 4]) ++ "\n"
+    get 2 2 = "?"
+    get x y = show $ fromMaybe Dead $ Map.lookup (level, V2 x y) tiles
--- aoc2019/test/AOC2019/Test/Day24.hs
+++ aoc2019/test/AOC2019/Test/Day24.hs
@@ -7,19 +7,20 @@ import           Test.HUnit
 import           AOC2019.Test.Common
 import           AOC2019.Day24
 
-day24tests = TestList [part1Test]--, part2Test]
+day24tests = TestList [part1Test, part2Test]
 
 part1Test :: Test
 part1Test =
   TestLabel "Day 24 - Part 1 " $ TestList $ map (testExample day24a) examples1
 
 part2Test :: Test
-part2Test =
-  TestLabel "Day 24 - Part 2 " $ TestList $ map (testExample day24b) examples2
+part2Test = TestLabel "Day 24 - Part 2 " $ TestList $ map
+  (testExample $ day24b 10)
+  examples2
 
 
 examples1 :: [(String, Int)]
 examples1 = [("....#\n#..#.\n#..##\n..#..\n#....\n", 2129920)]
 
 examples2 :: [(String, Int)]
-examples2 = undefined
+examples2 = [("....#\n#..#.\n#..##\n..#..\n#....\n", 99)]
